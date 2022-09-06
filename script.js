bcd26b30c61527f31b7f999d5a80ca8accfc4510
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-09-06 18:22:16 +0200

    script.js modified

--- script.js
+++ script.js
@@ -1,48 +1,59 @@
-const fetchJSON = () => {
-    const request = new XMLHttpRequest();
-    request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
-    request.send();
-    request.onload = function () {
-      // console.log(typeof request.responseText);
-      // console.log(request.responseText);
-      // console.log(JSON);
-      const data = JSON.parse(request.responseText);
-      console.log(data);
-      const jsonString = JSON.stringify(data);
-      console.log(jsonString);
-    };
-  };
-  fetchJSON();
-  // JEŻELI DOSTAJESZ RESPONSE OD SERWERA, TO DANE BĘDĄ W JSON
-  // ŻEBY PRACOWAĆ NA TYCH DANYCH W JS, MUSIMY NAJPIERW PRZEROBIĆ JSON NA ZWYKŁY JSOWY OBIEKT
-  // JSON.parse()
-  
-  // JEŻELI WYSYŁASZ DANE NA SERWER, TO MUSISZ ZADBAĆ O TO, ŻEBY WYSŁANE DANE NIE BYŁY OBIEKTEM JSOWYM, TYLKO STRINGIEM JSONOWYM
-  // JSON.stringify()
-  
-  const x = {
-    email: "1213@123.pl",
-    password: "12343",
-  };
-  // JSON.stringify()
-  // wysyłanie
+// const fetchJSON = () => {
+// 	//const request = new XMLHttpRequest();
+// 	request.open('GET', 'https://jsonplaceholder.typicode.com/todos/1');
+// 	request.send();
+// 	request.onload = function () {
+// 		// console.log(typeof request.responseText);
+// 		// console.log(request.responseText);
+// 		// console.log(JSON);
+// 		const data = JSON.parse(request.responseText);
+// 		console.log(data);
+// 		const jsonString = JSON.stringify(data);
+// 		console.log(jsonString);
+// 	};
+// };
+// fetchJSON();
+// JEŻELI DOSTAJESZ RESPONSE OD SERWERA, TO DANE BĘDĄ W JSON
+// ŻEBY PRACOWAĆ NA TYCH DANYCH W JS, MUSIMY NAJPIERW PRZEROBIĆ JSON NA ZWYKŁY JSOWY OBIEKT
+// JSON.parse()
 
-  //metody HTTP
+// JEŻELI WYSYŁASZ DANE NA SERWER, TO MUSISZ ZADBAĆ O TO, ŻEBY WYSŁANE DANE NIE BYŁY OBIEKTEM JSOWYM, TYLKO STRINGIEM JSONOWYM
+// JSON.stringify()
 
-  //1. Metoda GET
-  //pobieranie danych z serwera
-  //a) XMLHttpRequest
-  // https://jsonplaceholder.typicode.com/todos/
+// JSON.stringify()
+// wysyłanie
 
-    const XMLHttpRequest = () => { //reprezentacje requestów
-        //1. Stworzenie obiektu reQuesta
-        const request = new XMLHttpRequest()
-        //2. konfiguracja requesta przy pomocy open
-        //dwa wymagane argumenty
-        //metoda HTTP(np GET)
-        //url(endpoint jakiegoś API z którego chcemy pobrać dane)
-        request.open('GET', 'https://jsonplaceholder.typicode.com/todos/', true);
-    }
+//metody HTTP
 
-  //b) Fetch API + then chain
-  //c) Fetch API + async/await
+//1. Metoda GET
+//pobieranie danych z serwera
+//a) XMLHttpRequest
+// https://jsonplaceholder.typicode.com/todos/
+
+const XMLRequestGET = () => {
+	//reprezentacje requestów
+	//1. Stworzenie obiektu requesta
+	const request = new XMLHttpRequest();
+	//2. konfiguracja requesta przy pomocy open
+	//dwa wymagane argumenty
+	//- metoda HTTP(np GET)
+	//- url(endpoint jakiegoś API z którego chcemy pobrać dane)
+	request.open('GET', 'https://jsonplaceholder.typicode.com/todos/', true);
+
+	//3. wysłanie requesta(metodas send())
+	request.send();
+	//4. oczekiwanie i reakcja na response z serwera
+	request.onload = () => {
+		//reprezentacja requesta
+		// funkcja odpali się wtedy gry otrzymamy response z serwera
+		console.log(request);
+		console.log(request.responseTEXT);
+		const data = JSON.parse(request.responseText);
+		console.log(data);
+	};
+};
+XMLRequestGET();
+
+
+//b) Fetch API + then chain
+//c) Fetch API + async/await
